3ffa218676e185f81821d80ba90096d37bf66524
Fei ZHENG <feizhengprchina@hotmail.com>
2018-09-07 21:30:02 +0800

    framework of main procedure.

---
+++ gd_transfer/gd_transfer/src/main.cpp
@@ -0,0 +1,21 @@
+/*
+* Main procedure to make the transfer
+* from Gmsh file "0.geo"
+* to DDA program "0_block.json"
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	// [Screen Reminder].
+	cout << "-----\n";
+	cout << " Transfer Gmsh file to DDA block file (2d) ...\n";
+	cout << "-----\n";
+	
+	// [Main function return].
+	system("pause");
+	return 0;
+}
